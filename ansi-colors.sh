615a7069598700e00b03a3e78d0af024bed3bf9f
Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>
2024-01-13 08:07:12 +0300

    ansi-colors.sh: Add underlines
    Signed-off-by: Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>

--- ansi-colors.sh
+++ ansi-colors.sh
@@ -39,3 +39,54 @@ do
     printf "\x1B[0m"
     printf "\n"
 done
+
+# Underlines
+
+CNAMES=("BLK" "RED" "GRN" "YEL" "BLU" "MAG" "CYN" "WHT")
+
+printf "\033[1m" # bold
+printf "\nUnderline With FG Colors:\n"
+
+printf "\033[4m" # underline
+for foreground in $(seq 0 7)
+do
+    printf "\033[$((foreground+30))m ${CNAMES[$foreground]} "
+done
+printf "\x1B[24m\n" # no underline
+
+printf "\nUnderline Styles And Colors:\n"
+
+printf "\nFG:  "
+printf "\033[9mStrikeout\033[0m "
+printf "\033[4mUnderline\033[0m "
+printf "\033[4:2mDoubleUnderline\033[0m "
+printf "\033[4:3mCurlyUnderline\033[0m "
+printf "\033[4:4mDottedUnderline\033[0m "
+printf "\033[4:5mDashedUnderline\033[0m "
+printf "\n"
+
+printf "INV: "
+printf "\033[7m\033[9mStrikeout\033[0m "
+printf "\033[7m\033[4mUnderline\033[0m "
+printf "\033[7m\033[4:2mDoubleUnderline\033[0m "
+printf "\033[7m\033[4:3mCurlyUnderline\033[0m "
+printf "\033[7m\033[4:4mDottedUnderline\033[0m "
+printf "\033[7m\033[4:5mDashedUnderline\033[0m "
+printf "\n"
+
+for line_color in $(seq 0 7)
+do
+    printf "${CNAMES[$line_color]}: "
+    printf "          "
+    printf "\033[58:5:"${line_color}m
+    printf "\033[4mUnderline\033[0m "
+    printf "\033[58:5:"${line_color}m
+    printf "\033[4:2mDoubleUnderline\033[0m "
+    printf "\033[58:5:"${line_color}m
+    printf "\033[4:3mCurlyUnderline\033[0m "
+    printf "\033[58:5:"${line_color}m
+    printf "\033[4:4mDottedUnderline\033[0m "
+    printf "\033[58:5:"${line_color}m
+    printf "\033[4:5mDashedUnderline\033[0m "
+    printf "\n"
+done
