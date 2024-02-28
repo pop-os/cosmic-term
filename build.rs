efca9e7c228e5a69b68221436efb2154602a544d
Jeremy Soller <jackpot51@gmail.com>
2024-02-28 08:45:45 -0700

    Sync some changes from cosmic-edit

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Rebuild if i18n files change
+    println!("cargo:rerun-if-changed=i18n");
+
     vergen::EmitBuilder::builder()
         .fail_on_error()
         .git_commit_date()
