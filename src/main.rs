782bce1f011d2602dc9c78e1b1a3e020f62668ea
Jeremy Soller <jackpot51@gmail.com>
2024-04-05 19:27:21 -0600

    Enable CSDs on Redox

--- src/main.rs
+++ src/main.rs
@@ -141,13 +141,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut settings = Settings::default();
     settings = settings.theme(config.app_theme.theme());
-
-    #[cfg(target_os = "redox")]
-    {
-        // Redox does not support resize if doing CSDs
-        settings = settings.client_decorations(false);
-    }
-
     settings = settings.size_limits(Limits::NONE.min_width(360.0).min_height(180.0));
 
     let flags = Flags {
