d90fe53fab6f8bb82c0343f03c1bfea088950161
Daniel Eades <danieleades@hotmail.com>
2024-04-14 21:02:01 +0100

    remove needless 'return' statements (clippy::needless_return)

--- src/main.rs
+++ src/main.rs
@@ -1230,7 +1230,7 @@ impl App {
                 log::warn!("tried to create new tab before having event channel");
             }
         }
-        return self.update_title(Some(pane));
+        self.update_title(Some(pane))
     }
 }
 
