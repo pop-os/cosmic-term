ca2f5d9cb38bb5bb3d5d28fc986d491f2d26acf6
KENZ <KENZ.gelsoft@gmail.com>
2025-04-27 12:54:46 +0900

    WIP: Support CJK input method

--- src/terminal_box.rs
+++ src/terminal_box.rs
@@ -22,6 +22,7 @@ use cosmic::{
     iced_core::{
         Border, Shell,
         clipboard::Clipboard,
+        input_method::{self, InputMethod},
         keyboard::key::Named,
         layout::{self, Layout},
         renderer::{self, Quad, Renderer as _},
@@ -233,6 +234,19 @@ where
     pub fn disabled(mut self, disabled: bool) -> Self {
         self.disabled = disabled;
         self
+    }
+
+    fn input_method<'b>(&self, state: &'b State, layout: Layout<'_>) -> InputMethod<&'b str> {
+        if state.is_focused {
+        } else {
+            return InputMethod::Disabled;
+        }
+
+        InputMethod::Enabled {
+            position: Point::default(),
+            purpose: input_method::Purpose::Normal,
+            preedit: state.preedit.as_ref().map(input_method::Preedit::as_ref),
+        }
     }
 }
 
@@ -845,6 +859,11 @@ where
                         shell.publish(on_window_unfocused());
                     }
                 }
+                cosmic::iced::window::Event::RedrawRequested(_now) => {
+                    if state.is_focused {
+                        shell.request_input_method(&self.input_method(state, layout));
+                    }
+                }
                 _ => {}
             },
             Event::Keyboard(KeyEvent::KeyPressed {
@@ -1097,6 +1116,30 @@ where
                     }
                 }
             }
+            Event::InputMethod(event) => match event {
+                input_method::Event::Opened | input_method::Event::Closed => {
+                    state.preedit = matches!(event, input_method::Event::Opened).then(|| {
+                        let mut preedit = input_method::Preedit::new();
+                        preedit
+                    });
+                }
+                input_method::Event::Preedit(content, selection) => {
+                    if state.is_focused {
+                        state.preedit = Some(input_method::Preedit {
+                            content: content.to_owned(),
+                            selection: selection.clone(),
+                            // text_size: Some(self.metrics.font_size.into()),
+                            text_size: Some(10.0.into()),
+                        })
+                    }
+                }
+                input_method::Event::Commit(text) => {
+                    if state.is_focused {
+                        terminal.paste(text);
+                        status = Status::Captured;
+                    }
+                }
+            },
             Event::Mouse(MouseEvent::ButtonPressed(button)) => {
                 if let Some(p) = cursor_position.position_in(layout.bounds()) {
                     let x = p.x - self.padding.left;
@@ -1784,6 +1827,7 @@ pub struct State {
     scroll_pixels: f32,
     scrollbar_rect: Cell<Rectangle<f32>>,
     autoscroll: DragAutoscroll,
+    preedit: Option<input_method::Preedit>,
 }
 
 impl State {
@@ -1797,6 +1841,7 @@ impl State {
             scroll_pixels: 0.0,
             scrollbar_rect: Cell::new(Rectangle::default()),
             autoscroll: DragAutoscroll::new(AUTOSCROLL_INTERVAL),
+            preedit: None,
         }
     }
 }
